fa3e05ba8c35b6eb3bce64437879fd4e68d93961
Rakshit Poonia <rakshitpoonia2004@gmail.com>
2025-08-08 23:35:27 +0530

    file system in todo tasks read/write

---
+++ todo.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const filePath = "./tasks.json";
+
+const loadTasks = () => {
+  try {
+    const dataBuffer = fs.readFileSync(filePath);
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (error) {
+    return [];
+  }
+};
+
+const saveTasks = (tasks) => {
+  const dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const addTask = (task) => {
+  const tasks = loadTasks();
+  tasks.push({ task });
+  saveTasks(tasks);
+  console.log("Task added", task);
+};
+
+const listTasks = () => {
+  const tasks = loadTasks();
+  tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
+};
+
+// TODO: Remove task by index
+const removeTask = (index) => {
+  const tasks = loadTasks();
+  if (index < 1 || index > tasks.length) {
+    console.log("Invalid task number.");
+    return;
+  }
+  const removed = tasks.splice(index - 1, 1);
+  saveTasks(tasks);
+  console.log("Removed task:", removed[0].task);
+};
+
+const command = process.argv[2];
+const argument = process.argv[3];
+
+if (command === "add") {
+  addTask(argument);
+} else if (command === "list") {
+  listTasks();
+} else if (command === "remove") {
+  removeTask(parseInt(argument));
+} else {
+  console.log("Command not found!");
+}
